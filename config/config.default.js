b269ee6937f4408bc491a594f34b237f76f8760d
LouisTsang <junkun.zeng@qdtech.ai>
2021-03-15 10:41:09 +0800

    add:plugin-template

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * egg-chartexporter-plugin default config
+ * @member Config#chartexporterPlugin
+ * @property {String} SOME_KEY - some description
+ */
+exports.chartexporterPlugin = {
+
+};
